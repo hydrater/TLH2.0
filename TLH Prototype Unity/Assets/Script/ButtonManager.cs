349de03e24a9d4af08b47665081d47acaa0b4907
FantasyLance <fantasylance@gmail.com>
2016-12-13 21:48:57 +0800

    13/12/2016 9:48PM

---
+++ TLH Prototype Unity/Assets/Script/ButtonManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour {
+
+	public void startBtn(string startGame)
+    {
+        SceneManager.LoadScene(startGame);
+    }
+}
---
+++ TLH Prototype Unity/Assets/Script/NetworkManager.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkManager : MonoBehaviour
+{
+    bool dontdestroyonload = true;
+}
