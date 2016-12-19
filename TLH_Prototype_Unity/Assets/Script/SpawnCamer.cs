6c3212015a79d89a834a7725d792d7971e65a69b
FantasyLance <fantasylance@gmail.com>
2016-12-20 01:14:14 +0800

    Reimport some scripts and prefabs

---
+++ TLH_Prototype_Unity/Assets/Script/SpawnCamer.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnCamer : MonoBehaviour {
+
+	public GameObject FPScamera = null;
+
+	// Use this for initialization
+	void Start () {
+		Instantiate (FPScamera, this.transform.position, this.transform.rotation);
+	}
+}
---
+++ TLH_Prototype_Unity/Assets/Script/Testing.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class Testing : NetworkBehaviour {
+	
+	GameObject Tester;
+
+	// Use this for initialization
+	void Start () {
+		if (isLocalPlayer) {
+			GetComponent<vp_FPController> ().enabled = true;
+			GetComponent<vp_FPInput> ().enabled = true;
+			GetComponent<vp_FPPlayerEventHandler> ().enabled = true;
+			GetComponent<vp_WeaponHandler> ().enabled = true;
+			GetComponent<vp_SimpleCrosshair> ().enabled = true;
+			GetComponent<vp_FootstepManager> ().enabled = true;
+			GetComponent<vp_SimpleHUD> ().enabled = true;
+			GetComponent<vp_PainHUD> ().enabled = true;
+			GetComponent<vp_FPPlayerDamageHandler> ().enabled = true;
+			Camera.main.transform.position = this.transform.position - this.transform.forward*10 + this.transform.up * 5;
+			Camera.main.transform.LookAt(this.transform.position);
+			Camera.main.transform.parent = this.transform;
+		}
+	}
+
+}
